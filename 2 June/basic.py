ba8ebd50208a2f26cfa7dafa346cd4135ea6cfb3
Hemant Saini <hemantsaini09012004@gmail.com>
2025-06-13 15:45:37 +0530

    first commit

---
+++ 2 June/basic.py
@@ -0,0 +1,95 @@
+
+# name = "John"
+
+# print(type(name))
+
+# lastname = "Saini"
+# firstname = "Hemant"
+
+# print(firstname+" "+ lastname)
+
+# #indexing
+
+# print(firstname[2])
+
+# #slicing
+
+# print(firstname[0:3]) 
+# city = "Jaipur"
+# company = "Insfosis"
+
+# print(f"Hello I am from {city} , my company name is {company}")
+
+# print(len(company))
+# print(company.lower())
+# print(company.upper())
+
+# print(company.title())
+
+# print(company.capitalize())
+
+# print(company.swapcase())
+
+# print(company.count("s"))
+
+# print(company + city)
+# print(company + city)
+
+
+# type casting
+# name =[1,2,3,4,5]
+# print(name)
+# print(type(name))
+
+# #convert into set
+
+# name = set(name)
+
+
+
+# Python program to perform basic arithmetic operations
+
+# Input from user
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# # Perform operations
+# addition = num1 + num2
+# subtraction = num1 - num2
+# multiplication = num1 * num2
+
+# # Avoid division by zero
+# if num2 != 0:
+#     division = num1 / num2
+# else:
+#     division = "cannot divide by zero"
+
+# # Print results
+# print("Addition:", addition)
+# print("Subtraction:", subtraction)
+# print("Multiplication:", multiplication)
+# print("Division:", division)
+
+
+# # Logical operations with booleans
+# a = True
+# b = False
+
+# print("a AND b:", a and b)     # False
+# print("a OR b:", a or b)       # True
+# print("NOT a:", not a)         # False
+# print("a XOR b:", a != b)      # True
+
+# # task 2
+
+# dict = {
+#     "firset_name" : "hemant","city" : "Jaipur"
+# }
+# dict["first_name"]="Rahul"
+# dict["city"]="Udaipur"
+
+# # task 1 : Access the element 7 from a 
+tuple = (1,2,3,4,5,5,[45,7,8,8]) 
+a=tuple[6][1]
+print(a)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Demo
 this is my first repo
+
+hello app kaise ho
